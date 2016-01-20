d5ad27ae48931dfa463a124ddcc9d9873ae4b73c
Jacob Payne <jacobapayne@gmail.com>
2016-01-20 13:42:23 -0800

    First Steps

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,32 @@
 
 
+struct Petrovich {
+	gender: Gender
+}
+
+impl Petrovich {
+
+	fn new(gender: Gender) -> Petrovich {
+        Petrovich {
+			gender: gender
+		}
+	}
+}
+
+// Predefined Genders
+
+#[derive(PartialEq)]
+enum Gender {
+	Male,
+	Female,
+	Androgyenous
+}
 
 #[test]
 fn example_test(){
 
+    let male = Petrovich::new(Gender::Male);
+    assert!(male.gender == Gender::Female);
 }
 
 #[test]
