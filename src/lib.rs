003f33bec93943fab6e9250399090d6f1fa9e18f
Jacob Payne <jacobapayne@gmail.com>
2016-01-24 23:38:50 -0800

    Create Petrovich Generator

--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,94 @@
+extern crate yaml_rust;
+
+use std::io::prelude::*;
+use std::fs;
+use yaml_rust::{Yaml, YamlLoader};
+
+// Initializes, Stores and applies Rules
+struct Petrovich {
+    rules: Vec<Yaml>,
+}
+
+impl Petrovich {
+
+    fn new() -> Petrovich {
+
+        // Open Rules File (Panics on error)
+        let mut f = fs::File::open("./src/rules.yml").unwrap();
+        // Create String Buffer and Read to it
+        let mut buffer = String::new();
+        f.read_to_string(&mut buffer).unwrap();
+        // Pass Buffer to Yaml and unwrap
+        let data = YamlLoader::load_from_str(&buffer).unwrap();
+
+        // Return Petrovich with preloaded rules
+        Petrovich { rules: data }
+    }
+
+    // TODO
+    fn last_name(&self, person: &Person, word: &str, case: Case) -> String {
+        String::from(word)
+    }
+    // TODO
+    fn first_name(&self, person: &Person, word: &str, case: Case) -> String {
+        String::from(word)
+    }
+    // TODO
+    fn middle_name(&self, person: &Person, word: &str, case: Case) -> String {
+        String::from(word)
+    }
+}
 
 struct Person {
-	gender: Gender
+    gender: Gender,
 }
 
 impl Person {
-
-	fn new(gender: Gender) -> Person {
-        Person {
-			gender: gender
-		}
-	}
-
-	// fn last_name(self, word: &str, case: Case) -> String {}
-	// fn first_name(self, word: &str, case: Case) -> String {}
-	// fn middle_name(self, word: &str,case: Case) -> String {}
+    fn new(gender: Gender) -> Person {
+        Person { gender: gender }
+    }
 }
 
 // Predefined Genders
 
 #[derive(PartialEq)]
 enum Gender {
-	Male,
-	Female,
-	Androgyenous
+    Male,
+    Female,
+    Androgyenous,
 }
 
 // Predefined Cases
 
 #[derive(PartialEq)]
 enum Case {
-	Genitive,
-	Dative,
-	Accusative,
-	Instrumental,
-	Prepositional,
+    Genitive,
+    Dative,
+    Accusative,
+    Instrumental,
+    Prepositional,
 }
 
 #[test]
-fn should_create_person(){
-
+fn should_create_person() {
     let male = Person::new(Gender::Male);
     assert!(male.gender == Gender::Male);
+}
+
+#[test]
+fn should_case_name() {
+    let factory = Petrovich::new();
+    let male = &Person::new(Gender::Male);
+
+    assert_eq!("Лёша",
+               factory.first_name(male, "Лёша", Case::Genitive));
+    assert_eq!("Лёше",
+               factory.first_name(male, "Лёша", Case::Dative));
+    assert_eq!("Лёшу",
+               factory.first_name(male, "Лёша", Case::Accusative));
+    assert_eq!("Лёшой",
+               factory.first_name(male, "Лёша", Case::Instrumental));
+    assert_eq!("Лёше",
+               factory.first_name(male, "Лёша", Case::Prepositional));
+
 }
