edc4a51c3bb79fe1db9f1109d21bde41c5c6a145
Jacob Payne <jacobapayne@gmail.com>
2016-02-08 00:11:01 -0800

    Match Exceptions

--- src/lib.rs
+++ src/lib.rs
@@ -82,31 +82,55 @@ impl Petrovich {
         // First Let's Check for Exceptions
         let exceptions = self.firstname["exceptions"].as_vec().unwrap();
 
+        // Search Exceptions with matching of name and gender
         let exception = exceptions.into_iter().find(|exception| {
-            exception["test"].as_str().unwrap() == name &&
-            exception["gender"].as_str().unwrap() == gender.as_str()
+
+            // Check if name matches
+            let match_test = exception["test"].as_vec().unwrap().into_iter().any(|test| {
+                test.as_str().unwrap() == name.to_lowercase()
+            });
+            // Check if gender matches
+            let match_gender = exception["gender"].as_str().unwrap() == gender.as_str();
+            // Return true if both match
+            match_test && match_gender
         });
 
-        // If Exeption Rule is found
+        // If Exception Rule is found
         if let Some(rule) = exception {
             
+            // Apply Rule
+
+            // First unwrap to vector Vec<&str>
+            let inflections = rule["mods"].as_vec().unwrap();
+
+            // Get inflection by Case
+            let inflection = inflections[case as usize].as_str().unwrap();
+
+            //// Parse Inflection
+            // Count amount of dashes: "-"
+            let postdelete: usize = inflection.rfind("-").map_or(0, |pos| pos+1);
+            // Get Postfix
+            let postfix = inflection.trim_left_matches("-");
+
+            // Apply Inflection
+            name[..(name.len() - postdelete)].to_string().push_str(postfix)
         }
 
         // If No Exceptions Matched we Check for Suffixes
 
         // Once the correct rule is found we apply the rule
 
-        String::from(name)
+        String::from("")
     }
 
     // TODO
     fn middle_name(&self, gender: Gender, name: &str, case: Case) -> String {
-        String::from(name)
+        String::from("")
     }
 
     // TODO
     fn last_name(&self, gender: Gender, name: &str, case: Case) -> String {
-        String::from(name)
+        String::from("")
     }
 }
 
@@ -119,18 +143,19 @@ fn should_initialize() {
 fn should_inflect_first_name() {
     let factory = Petrovich::new();
 
-    assert_eq!("Лёши",
-               factory.first_name(Gender::Male, "Лёша", Case::Genitive));
-    assert_eq!("Лёше",
-               factory.first_name(Gender::Male, "Лёша", Case::Dative));
-    assert_eq!("Лёшу",
-               factory.first_name(Gender::Male, "Лёша", Case::Accusative));
-    assert_eq!("Лёшой",
-               factory.first_name(Gender::Male, "Лёша", Case::Instrumental));
-    assert_eq!("Лёше",
-               factory.first_name(Gender::Male, "Лёша", Case::Prepositional));
-
-     assert_eq!("Яши",
+    // // Лёша
+    // assert_eq!("Лёши",
+    //            factory.first_name(Gender::Male, "Лёша", Case::Genitive));
+    // assert_eq!("Лёше",
+    //            factory.first_name(Gender::Male, "Лёша", Case::Dative));
+    // assert_eq!("Лёшу",
+    //            factory.first_name(Gender::Male, "Лёша", Case::Accusative));
+    // assert_eq!("Лёшой",
+    //            factory.first_name(Gender::Male, "Лёша", Case::Instrumental));
+    // assert_eq!("Лёше",
+    //            factory.first_name(Gender::Male, "Лёша", Case::Prepositional));
+    
+    assert_eq!("Яши",
                factory.first_name(Gender::Male, "Яша", Case::Genitive));
     assert_eq!("Яше",
                factory.first_name(Gender::Male, "Яша", Case::Dative));
